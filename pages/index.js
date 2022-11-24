8f25caa733027370da475141164ae3ae9143703e
Roni Wellington <roniwellington@gmail.com>
2022-11-24 18:33:29 -0300

    função do butto concluido

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,18 @@
 import React from "react";
 import Config from "../config.json";
 import styled from "styled-components";
-import { CSSReset } from "../src/components/CSSReset";
 import Menu from "../src/components/Menu";
 import { StyledTimeline } from "../src/components/Timeline"
 
 function HomePage() {
-    const estilosDaHomePage = {
-        // backgroundColor: "red"
-    };
     const [valorDoFiltro, setValorDoFiltro] = React.useState("");
 
     return (
-        <>
-            <CSSReset />
+        <>       
             <div style={{
                 display: "flex",
                 flexDirection: "column",
                 flex: 1,
-
             }}>
                 <Menu valorDoFiltro={valorDoFiltro} setValorDoFiltro={setValorDoFiltro} />
                 <Header />
@@ -42,13 +36,14 @@ export default HomePage
 
 
 const StyledHeader = styled.div`
+    background-color: ${({ theme }) => theme.backgroundLevel1};
+
     img{
         width: 80px;
         height: 80px;
         border-radius: 50%;
     }
     .user-info {
-        margin-top: 50px;
         display: flex;
         align-items: center;
         width: 100%;
